216900af3160b2d90d4c6513fc9378579699c532
shiningedge-143 <75673573+shiningedge-143@users.noreply.github.com>
2020-12-28 21:23:58 +0800

    Practice

---
+++ my.js
@@ -0,0 +1,11 @@
+// let addtoDoButton = document.getElementById('addButton');
+// let toDoContainer = document.getElementById('toDoContainer');
+// let toDoInput = document.getElementById('toDoInput')
+
+// addtoDoButton.addEventListener('click', function(){
+//     var paragraph = document.createElement('p');
+//     paragraph.innerText = toDoInput.value;
+//     toDoContainer.appendChild(paragraph);
+  
+// })
+
